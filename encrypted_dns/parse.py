adfa3288bdd5045cbddd6478f8d22112ebe1dda0
Siujoeng Lau <admin@xasiimov.com>
2019-08-12 21:50:35 -0700

    Implement a function which could cast a byte to a bit list. Fix bugs in parsing DNS query header. Implement functions which could parse question section in DNS query.
    Signed-off-by: Siujoeng Lau <admin@xasiimov.com>

--- encrypted_dns/parse.py
+++ encrypted_dns/parse.py
@@ -10,21 +10,24 @@ class ParseQuery:
 
         header = {
             'transaction_id': query_data[:2],
-            'flags': self._parse_flags(query_data[2:4]),
-            'question_count': query_data[4:6],
-            'answer_count': query_data[6:8],
-            'name_server_count': query_data[8:10],
-            'additional_record__count': query_data[10:12]
+            'flags': self.parse_flags(query_data[2:4]),
+            'question_count': int.from_bytes(query_data[4:6], byteorder='big'),
+            'answer_count': int.from_bytes(query_data[6:8], byteorder='big'),
+            'name_server_count': int.from_bytes(query_data[8:10], byteorder='big'),
+            'additional_record__count': int.from_bytes(query_data[10:12], byteorder='big')
         }
 
-        question = self._parse_question(query_data[12:])
+        question = self.parse_question(query_data[12:])
         answer = {}
 
         parsed_result = [header, question, answer]
         return parsed_result
 
     @staticmethod
-    def _parse_flags(flags_data):
+    def parse_flags(flags_data):
+        print(int.from_bytes(flags_data[:1], byteorder='big'), int.from_bytes(flags_data[1:2], byteorder='big'))
+        print(bin(int.from_bytes(flags_data[:1], byteorder='big')),
+              bin(int.from_bytes(flags_data[1:2], byteorder='big')))
         qr = utils.get_bit_from_byte(flags_data[:1], 0)
         qpcode = utils.get_bit_from_byte(flags_data[:1], 1, 4)
         aa = utils.get_bit_from_byte(flags_data[:1], 5)
@@ -48,7 +51,7 @@ class ParseQuery:
         return flags
 
     @staticmethod
-    def _parse_question(question_data):
+    def parse_question(question_data):
         state = False
         expected_length = 0
         domain_string = ''
@@ -57,8 +60,6 @@ class ParseQuery:
         pointer = 0
 
         for byte in question_data:
-            print(chr(byte))
-            print(pointer)
             if state:
                 pointer += 1
                 domain_string += chr(byte)
@@ -79,13 +80,59 @@ class ParseQuery:
             domain_end_point += 1
 
         qname = domain_parts
-        qtype = question_data[domain_end_point + 1:domain_end_point + 3]
+        qtype = question_data[domain_end_point:domain_end_point + 2]
+        qclass = question_data[domain_end_point + 2: domain_end_point + 4]
 
         question = {
             'QNAME': qname,
-            'QTYPE': qtype
+            'QTYPE': ParseQuery.get_query_type(int.from_bytes(qtype, byteorder='big')),
+            'QCLASS': ParseQuery.get_query_class(int.from_bytes(qclass, byteorder='big'))
         }
         return question
+
+    @staticmethod
+    def get_query_type(qtype):
+        qtype_dict = {
+            1: 'A',
+            2: 'NS',
+            3: 'MD',
+            4: 'MF',
+            5: 'CNAME',
+            6: 'SOA',
+            7: 'MB',
+            8: 'MG',
+            9: 'MR',
+            10: 'NULL',
+            11: 'WKS',
+            12: 'PTR',
+            13: 'HINFO',
+            14: 'MINFO',
+            15: 'MX',
+            16: 'TXT',
+            28: 'AAAA',
+            33: 'SRV',
+            35: 'NAPTR',
+            48: 'DNSKEY',
+            250: 'TSIG',
+            252: 'AXFR',
+            253: 'MAILB',
+            254: 'MAILA',
+            255: '*',
+            256: 'URI'
+        }
+
+        return qtype_dict[qtype]
+
+    @staticmethod
+    def get_query_class(qclass):
+        qclass_dict = {
+            1: 'In',
+            2: 'CS',
+            3: 'CH',
+            4: 'HS'
+        }
+
+        return qclass_dict[qclass]
 
 
 class ParseResponse:
