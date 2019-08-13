5d9c362a2663ea3745e2e48d545ea869996ac362
Xiaoyang Liu <siujoeng.lau@gmail.com>
2019-08-12 21:50:35 -0700

    Implement a function which could cast a byte to a bit list. Fix bugs in parsing DNS query header. Implement functions which could parse question section in DNS query.
    Signed-off-by: Siujoeng Lau <admin@xasiimov.com>

--- encrypted_dns/utils.py
+++ encrypted_dns/utils.py
@@ -1,8 +1,16 @@
 def get_bit_from_byte(byte, begin, end=None):
+    integer = int.from_bytes(byte, byteorder='big')
+    bin_list = [int(x) for x in bin(integer)[2:]]
+    for i in range(8 - len(bin_list)):
+        bin_list.insert(0, 0)
+
     if end is None:
         end = begin
 
-    result = ''
-    for bit in range(begin, end + 1):
-        result += str(ord(byte) & (1 << bit))
+    result_list = bin_list[begin:end + 1]
+    result = ''.join(str(x) for x in result_list)
+
+    # result = ''
+    # for bit in range(begin, end + 1):
+    #     result += str(ord(byte) & (1 << bit))
     return result
