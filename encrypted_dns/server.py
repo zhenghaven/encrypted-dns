a1855adad3fec9b9dd38aba660fd607bc920866a
Xiaoyang Liu <siujoeng.lau@gmail.com>
2019-08-14 12:46:20 -0700

    Implement PlainUpstream class for upstream.py. Add a dict to map response data to query address by using transaction_id as key and query_address as value.
    Signed-off-by: Siujoeng Lau <admin@xasiimov.com>

--- encrypted_dns/server.py
+++ encrypted_dns/server.py
@@ -5,31 +5,52 @@ from encrypted_dns import parse, upstream
 
 class Server:
 
-    def __init__(self, ip='127.0.0.1', port=10053):
+    def __init__(self, ip="0.0.0.0", port=10053):
         self.ip = ip
         self.port = port
         self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.dns_map = {}
 
     def start(self):
         self.server.bind((self.ip, self.port))
 
         while True:
-            query_data, query_address = self.server.recvfrom(512)
-            print('query_data:', query_data)
-            self.handle_query(query_data, query_address)
+            recv_data, recv_address = self.server.recvfrom(512)
+            recv_header = parse.ParseHeader.parse_header(recv_data)
+            print('recv_data:', recv_data)
+
+            transaction_id = recv_header['transaction_id']
+            transaction_id = transaction_id.hex()
+            print('transaction_id:', transaction_id)
+
+            if recv_header['flags']['QR'] == '0':
+                self.dns_map[transaction_id] = recv_address
+                self.handle_query(recv_data)
+
+            if recv_header['flags']['QR'] == '1':
+                if transaction_id in self.dns_map:
+                    sendback_address = self.dns_map[transaction_id]
+                    self.server.sendto(recv_data, sendback_address)
+                    self.dns_map.pop(transaction_id)
+                else:
+                    pass
+
+                self.handle_response(recv_data)
 
     def _send(self, response_data, address):
         self.server.sendto(response_data, address)
 
-    def handle_query(self, query_data, query_address):
+    def handle_query(self, query_data):
         query_parser = parse.ParseQuery(query_data)
         parse_result = query_parser.parse_plain()
         print('parse_result:', parse_result)
 
-        https_upstream = upstream.HTTPSUpsream('https://cloudflare-dns.com/dns-query?')
-        response = https_upstream.query(query_data)
-        self.server.sendto(response, query_address)
+        # https_upstream = upstream.HTTPSUpstream(self.server, 'https://1.1.1.1/dns-query?')
+        # https_upstream.query(query_data)
+        plain_upstream = upstream.PlainUpstream(self.server, '1.1.1.1')
+        plain_upstream.query(query_data)
 
+    @staticmethod
+    def handle_response(self):
+        pass
 
-test_server = Server()
-test_server.start()
--- encrypted_dns/upstream.py
+++ encrypted_dns/upstream.py
@@ -1,27 +1,31 @@
 import base64
-import socket
 import urllib.parse
 import urllib.request
 
 
 class PlainUpstream:
-    def __init__(self, upstream_ip, upstream_port=53):
+    def __init__(self, client, upstream_ip, upstream_port=53):
         self.upstream_ip = upstream_ip
         self.upsream_port = upstream_port
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.client = client
+
+    def query(self, query_data):
+        self._send(query_data)
 
     def _send(self, message_data):
         self.client.sendto(message_data, (self.upstream_ip, self.upsream_port))
 
 
-class HTTPSUpsream:
-    def __init__(self, upstream_url):
+class HTTPSUpstream:
+    def __init__(self, client, upstream_url):
         self.upstream_url = upstream_url
+        self.client = client
 
     def query(self, query_data):
         base64_query_string = self.struct_query(query_data)
-        base64_query_string = base64_query_string.replace('=', '', 2)
-        base64_query_string = base64_query_string.replace('+', '', 2)
+        base64_query_string = base64_query_string.replace('=', '')
+        base64_query_string = base64_query_string.replace('+', '-')
+        base64_query_string = base64_query_string.replace('/', '_')
         print('base64_query_string:', base64_query_string)
 
         query_parameters = urllib.parse.urlencode({'dns': base64_query_string, 'ct': 'application/dns-message'})
@@ -33,7 +37,8 @@ class HTTPSUpsream:
         response_data = urllib.request.urlopen(query_request)
         query_result = response_data.read()
         print('query_result: ', query_result)
-        return query_result
+
+        self.client.sendto(query_result, ('127.0.0.1', 53))
 
     @staticmethod
     def struct_query(query_data):
