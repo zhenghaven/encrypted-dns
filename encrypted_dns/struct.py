8299a5b2d9fbfaf07fa4feb8a9a93c2e657e6956
Siujoeng Lau <admin@xasiimov.com>
2019-09-02 15:14:58 +0800

    Implement method StructQuery.struct_question. Fix minor bugs.
    Signed-off-by: Siujoeng Lau <admin@xasiimov.com>

--- encrypted_dns/struct.py
+++ encrypted_dns/struct.py
@@ -12,16 +12,17 @@ class StructQuery:
     def struct(self):
         query_data = bytes()
         header = self.struct_header()
-
+        question = self.struct_question(self.address, self.query_type)
         query_data += header
-        return query_data
+        query_data += question
+        return query_data, self.transaction_id
 
     def struct_header(self, qr=0, opcode=0, aa=0, tc=0, rd=1, ra=0, z=0,
                       rcode=0, qcount=1, ancount=0, nscount=0, arcount=0):
         header = bytes(self.transaction_id)
         bit_cache = list()
         bit_cache.append(qr)
-        bit_cache.append(utils.get_bit_list_from_integer(opcode, 4))
+        bit_cache = bit_cache + utils.get_bit_list_from_integer(opcode, 4)
         bit_cache.append(aa)
         bit_cache.append(tc)
         bit_cache.append(rd)
@@ -29,16 +30,31 @@ class StructQuery:
 
         bit_cache = list()
         bit_cache.append(ra)
-        bit_cache.append(utils.get_bit_list_from_integer(z, 3))
-        bit_cache.append(utils.get_bit_list_from_integer(rcode, 4))
+        bit_cache = bit_cache + utils.get_bit_list_from_integer(z, 3)
+        bit_cache = bit_cache + utils.get_bit_list_from_integer(rcode, 4)
         header += utils.get_bytes_from_bits(bit_cache)
 
         header += bytes([qcount, ancount, nscount, arcount])
 
         return header
 
-    def struct_question(self, qname, qtype, qclass=1):
-        pass
+    @staticmethod
+    def struct_question(qname_string, qtype, qclass='IN'):
+        question = bytearray()
+        qname_string = qname_string.split('.')
+
+        for split_name in qname_string:
+            question.append(len(split_name))
+            question.extend(bytearray(split_name, 'utf-8'))
+
+        question.append(0x00)
+
+        qtype = utils.get_record_type(qtype)
+        question.append(qtype)
+
+        qclass = utils.get_record_class(qclass)
+        question.append(qclass)
+        return question
 
 
 class StructResponse:
