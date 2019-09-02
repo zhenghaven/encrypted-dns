bf276df7da1ef1edbb1ef0c1e4515ad2b0f8c013
Siujoeng Lau <admin@xasiimov.com>
2019-09-02 09:36:18 +0800

    Add methods to utils.py to convert binary code to dns record type and class. Fix minor bugs and typos in upstream.py.
    Signed-off-by: Siujoeng Lau <admin@xasiimov.com>

--- encrypted_dns/utils.py
+++ encrypted_dns/utils.py
@@ -21,6 +21,7 @@ def get_bit_list_from_integer(n, length):
             bit_list.insert(0, 0)
     return bit_list
 
+
 def get_bytes_from_bits(bits):
     bits = iter(bits)
     done = False
@@ -48,7 +49,94 @@ def get_bit_from_byte(byte, begin, end=None):
     result_list = bin_list[begin:end + 1]
     result = ''.join(str(x) for x in result_list)
 
-    # result = ''
-    # for bit in range(begin, end + 1):
-    #     result += str(ord(byte) & (1 << bit))
+    return result
+
+
+def get_domain_name_from_question_data(question_data):
+    state = False
+    expected_length = 0
+    domain_string = ''
+    domain_parts = []
+    domain_end_point = 0
+    pointer = 0
+
+    for byte in question_data:
+        if state:
+            pointer += 1
+            domain_string += chr(byte)
+
+            if pointer == expected_length:
+                domain_parts.append(domain_string)
+                domain_string = ''
+                state = False
+                pointer = 0
+
+            if byte == 0:
+                domain_parts.append(domain_string)
+                break
+        else:
+            state = True
+            expected_length = byte
+
+        domain_end_point += 1
+
+    return domain_parts, domain_end_point
+
+
+def get_record_type(record_type):
+    record_type_dict = {
+        1: 'A',
+        2: 'NS',
+        3: 'MD',
+        4: 'MF',
+        5: 'CNAME',
+        6: 'SOA',
+        7: 'MB',
+        8: 'MG',
+        9: 'MR',
+        10: 'NULL',
+        11: 'WKS',
+        12: 'PTR',
+        13: 'HINFO',
+        14: 'MINFO',
+        15: 'MX',
+        16: 'TXT',
+        28: 'AAAA',
+        33: 'SRV',
+        35: 'NAPTR',
+        48: 'DNSKEY',
+        250: 'TSIG',
+        252: 'AXFR',
+        253: 'MAILB',
+        254: 'MAILA',
+        255: '*',
+        256: 'URI'
+    }
+
+    result = None
+    if isinstance(record_type, int):
+        result = record_type_dict[record_type]
+    elif isinstance(record_type, str):
+        reverse_dict = dict((v, k) for k, v in record_type_dict.items())
+        result = reverse_dict[record_type]
+
+    return result
+
+
+def get_record_class(record_class):
+    record_class_dict = {
+        1: 'IN',
+        2: 'CS',
+        3: 'CH',
+        4: 'HS',
+        255: '*'
+    }
+
+    result = None
+    if isinstance(record_class, int):
+        result = record_class_dict[record_class]
+    elif isinstance(record_class, str):
+        reverse_dict = dict((v, k) for k, v in record_class_dict.items())
+        result = reverse_dict[record_class]
+
     return result
