44088690611507197640341fe950ee41e576ea16
Siujoeng Lau <admin@siujoeng-lau.com>
2019-10-25 11:55:55 +0800

    Refactoring log.py to support log writer.
    Signed-off-by: Siujoeng Lau <admin@siujoeng-lau.com>

--- encrypted_dns/__init__.py
+++ encrypted_dns/__init__.py
@@ -1,5 +1,6 @@
 import encrypted_dns.parse as parse
 import encrypted_dns.utils as utils
+import encrypted_dns.log as log
 
 from encrypted_dns.config import Config
 from encrypted_dns.server import Server
