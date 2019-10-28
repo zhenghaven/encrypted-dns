450a340d926aed0648024b37b60e7b5fe4813a70
Xiaoyang Liu <siujoeng.lau@gmail.com>
2019-10-28 16:35:30 +0800

    Refactoring directory structures and update .gitignore.
    Signed-off-by: Siujoeng Lau <admin@siujoeng-lau.com>

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import encrypted_dns
 
 dns_config_object = encrypted_dns.Config()
+print("Encrypted-DNS Resolver Started")
 dns_server = encrypted_dns.Server(dns_config_object)
-print("Encrypted-DNS Resolver Started.")
 dns_server.start()
